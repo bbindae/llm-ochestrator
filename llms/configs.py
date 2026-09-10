4050a497bf6d4382145639387108e6d41fc7b474
bbindae <bbindae@hotmail.com>
2026-09-10 15:13:26 -0700

    save-load

--- llms/configs.py
+++ llms/configs.py
@@ -1,13 +1,12 @@
 model_configs = {
-    "gpt2-small (124M)": {"model_name": "gpt2-small", "model_size": "124M", "emb_dim": 768, "n_layers": 12, "n_heads": 12},
-    "gpt2-medium (355M)": {"model_name": "gpt2-medium", "model_size": "355M", "emb_dim": 1024, "n_layers": 24, "n_heads": 16},
-    "gpt2-large (774M)": {"model_name": "gpt2-large", "model_size": "774M", "emb_dim": 1280, "n_layers": 36, "n_heads": 20},
-    "gpt2-xl (1558M)": {"model_name": "gpt2-xl", "model_size": "1558M", "emb_dim": 1600, "n_layers": 48, "n_heads": 25},
+    "gpt2-small (124M)": {"model_name": "gpt2-small", "model_size": "124M", "emb_dim": 768, "n_layers": 12, "n_heads": 12, "context_length": 256, "drop_rate": 0.1, "qkv_bias": False},
+    "gpt2-medium (355M)": {"model_name": "gpt2-medium", "model_size": "355M", "emb_dim": 1024, "n_layers": 24, "n_heads": 16, "context_length": 1024, "drop_rate": 0.0, "qkv_bias": True},
+    "gpt2-large (774M)": {"model_name": "gpt2-large", "model_size": "774M", "emb_dim": 1280, "n_layers": 36, "n_heads": 20, "context_length": 1024},
+    "gpt2-xl (1558M)": {"model_name": "gpt2-xl", "model_size": "1558M", "emb_dim": 1600, "n_layers": 48, "n_heads": 25, "context_length": 1024},
 }
 
 BASE_CONFIG = {
-    "vocab_size": 50257,     # Vocabulary size
-    "context_length": 1024,  # Context length
+    "vocab_size": 50257,     # Vocabulary size    
     "drop_rate": 0.0,        # Dropout rate
     "qkv_bias": True         # Query-key-value bias
 }
--- llms/llm_ochestrator.py
+++ llms/llm_ochestrator.py
@@ -5,6 +5,7 @@ from llms.configs import get_config
 from llms.models import GPTModel
 from llms.gpt_download import download_and_load_gpt2
 
+import llms.data_loaders as data_loaders
 import llms.utils as utils
 
 
@@ -14,9 +15,10 @@ class LLMOchestrator():
 
     def __init__(self, config_name):
         self.model_config = get_config(config_name)
-        self.model = GPTModel(self.model_config)        
         self.device = utils.get_device()
+        self.model = GPTModel(self.model_config).to(self.device)                
         self.tokenizer = tiktoken.get_encoding("gpt2")
+        self.optimizer = torch.optim.AdamW(self.model.parameters(), lr=0.0004, weight_decay=0.1)
 
     def load_weight(self):
         settings, params = download_and_load_gpt2(
@@ -25,10 +27,37 @@ class LLMOchestrator():
                 
         utils.load_weights_into_gpt(self.model, params)
 
+    def save_model(self, model_path):
+            torch.save({
+                "model_state_dict": self.model.state_dict(),
+                "optimizer_state_dict": self.optimizer.state_dict(),
+                "optimizer_class": self.optimizer.__class__.__name__
+                },
+                model_path)   
+
+
     def load_model(self, model_path):
-        self.model.load_state_dict(torch.load(model_path, 
-                                              map_location=self.device, 
-                                              weights_only=True))
+        checkpoint = torch.load(model_path,
+                                weights_only=False,
+                                map_location=self.device)
+
+        
+        self.model.load_state_dict(checkpoint["model_state_dict"])
+    
+        opt_state = checkpoint["optimizer_state_dict"]
+        opt_class_name = checkpoint.get("optimizer_class", "AdamW")
+        opt_class = getattr(torch.optim, opt_class_name)
+
+        param_group = opt_state["param_groups"][0]
+        saved_lr = param_group.get("lr", 0.0004)
+        saved_weight_decay = param_group.get("weight_decay", 0.1)
+
+        self.optimizer = opt_class(self.model.parameters(), 
+                                   lr=saved_lr,
+                                   weight_decay=saved_weight_decay)  
+
+        # TODO: write code when "otimizer_clas is not available"              
+        
 
     def create_model(self, model_path, config=None):
         pass
@@ -37,17 +66,92 @@ class LLMOchestrator():
         pass
 
     def save_model(self, model_path):
-        torch.save(self.model.state_dict(), model_path)
+        torch.save({
+            "model_state_dict": self.model.state_dict(),
+            "optimizer_state_dict": self.optimizer.state_dict(),
+            "optimizer_class": self.optimizer.__class__.__name__
+            },
+            model_path)
 
-    def generate_text(self, start_context, max_new_tokens):
-        token_ids = utils.generate_text_simple(
+    def generate_text(self, start_context, max_new_tokens, temperature=0.0, top_k=None, eos_id=None):
+        token_ids = utils.generate_text(
             model=self.model,
             max_new_tokens=max_new_tokens,
-            idx=utils.text_to_token_ids(start_context, self.tokenizer),
-            context_size=self.model_config["context_length"]
+            idx=utils.text_to_token_ids(start_context, self.tokenizer).to(self.device),
+            context_size=self.model_config["context_length"],
+            temperature=temperature,
+            top_k=top_k,
+            eos_id=eos_id
         )
 
         return utils.token_ids_to_text(token_ids, self.tokenizer)
+
+    def create_plantext_dataloader(self, file_path, train_ratio=0.7, validation_ratio=0.2, batch_size=4, shuffle=True, drop_last=True, num_workers=0):
+                
+        with open(file_path, "r", encoding="utf-8") as file:
+            text_data = file.read()
+
+        train_end = int(len(text_data) * train_ratio)
+        train_data = text_data[:train_end]
+        train_loader = data_loaders.create_plantext_dataloader(train_data, self.tokenizer,
+                                                               batch_size=batch_size, max_length=self.model_config["context_length"],
+                                                               stride=self.model_config["context_length"], shuffle=shuffle, drop_last=False,
+                                                               num_workers=num_workers)
+
+        validation_end = train_end + int(len(text_data) * validation_ratio)
+        validation_data = text_data[train_end:validation_end]
+        validation_loader = data_loaders.create_plantext_dataloader(validation_data, self.tokenizer,
+                                                                    batch_size=batch_size, max_length=self.model_config["context_length"],
+                                                                    stride=self.model_config["context_length"], shuffle=False, drop_last=False,
+                                                                    num_workers=num_workers)      
+
+        test_loader = None
+
+        if train_ratio + validation_ratio < 1:
+            test_data = text_data[validation_end:]
+            test_loader = data_loaders.create_plantext_dataloader(test_data, self.tokenizer,
+                                                                    batch_size=batch_size, max_length=self.model_config["context_length"],
+                                                                    stride=self.model_config["context_length"], shuffle=False, drop_last=drop_last,
+                                                                    num_workers=num_workers)
+               
+
+        return train_loader, validation_loader, test_loader
+
+    def train_model_by_plantext(self, num_epochs=10, eval_freq=5, eval_iter=5,start_context="Hello World", 
+                                train_loader=None, validation_loader=None, batch_size = None, file_path=None):
+
+        if train_loader == None or validation_loader == None:
+            if batch_size == None:
+                raise ValueError("batch_size should be specified")
+            train_loader, validation_loader, test_loader = self.create_plantext_dataloader(file_path=file_path, train_ratio=0.7, validation_ratio=0.2,
+                                            batch_size=batch_size)
+
+        train_losses, val_losses, tokens_seen = utils.train_model_simple(self.model, train_loader=train_loader, val_loader=validation_loader,
+                                 optimizer=self.optimizer, device=self.device, num_epochs=num_epochs,
+                                 eval_freq=eval_freq, eval_iter=eval_iter, start_context=start_context,
+                                 tokenizer=self.tokenizer)
+
+        self.num_epochs = num_epochs
+        self.train_losses = train_losses
+        self.val_losses = val_losses
+        self.tokens_seen = tokens_seen
+
+        return train_losses, val_losses, tokens_seen
+
+    def finetune_model_by_plantext(self):
+        pass
+
+    def plot_losses(self, num_epochs=None, tokens_seen=None, train_losses=None, val_losses=None):
+
+        if num_epochs==None or tokens_seen==None or train_losses==None or val_losses==None:
+            num_epochs = self.num_epochs
+            tokens_seen = self.tokens_seen
+            train_losses = self.train_losses
+            val_losses = self.val_losses
+
+        epochs_tensor = torch.linspace(0, num_epochs, len(train_losses))
+        utils.plot_losses(epochs_tensor, tokens_seen, train_losses, val_losses)
+        
     
 
 
